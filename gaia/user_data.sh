950d673c6420f54c6d4492feabfd48b6ff83e301
Ryan J. Price <ryapric@gmail.com>
2021-10-11 19:37:19 -0500

    Need to change ubuntu -> admin

--- gaia/user_data.sh
+++ gaia/user_data.sh
@@ -12,30 +12,30 @@ swapon /swap
 
 # Install container runtime & build base game server image
 curl -fsSL https://get.docker.com | bash
-usermod -aG docker ubuntu
+usermod -aG docker admin
 
-git clone https://github.com/ryapric/game-servers.git /home/ubuntu/game-servers
-chown -R ubuntu:ubuntu /home/ubuntu
+git clone https://github.com/ryapric/game-servers.git /home/admin/game-servers
+chown -R admin:admin /home/admin
 
-docker build -t ryapric/game-servers:latest /home/ubuntu/game-servers
+docker build -t ryapric/game-servers:latest /home/admin/game-servers
 
 # Grab backup data if not present
 apt-get update && apt-get install -y awscli
 account_id=$(aws sts get-caller-identity --query 'Account' --output text)
-if [[ ! -d /home/ubuntu/game-data ]]; then
-  aws s3 cp s3://ryapric-game-servers-"${account_id}"/backups.tar.gz /home/ubuntu/backups.tar.gz
-  tar -v -C /home/ubuntu -xzf /home/ubuntu/backups.tar.gz
+if [[ ! -d /home/admin/game-data ]]; then
+  aws s3 cp s3://ryapric-game-servers-"${account_id}"/backups.tar.gz /home/admin/backups.tar.gz
+  tar -v -C /home/admin -xzf /home/admin/backups.tar.gz
 fi
 
 # Set up backup script
 cat <<EOF > /usr/local/bin/backup_game_data
 #!/usr/bin/env bash
-cd /home/ubuntu
+cd /home/admin
 tar -czf backups.tar.gz ./game-data
 aws s3 cp ./backups.tar.gz s3://ryapric-game-servers-${account_id}/backups.tar.gz
 EOF
 chmod +x /usr/local/bin/backup_game_data
-echo '0 * * * * root /bin/bash /usr/local/bin/backup_game_data > /home/ubuntu/cron.log 2>&1' > /etc/cron.d/backup_game_data
+echo '0 * * * * root /bin/bash /usr/local/bin/backup_game_data > /home/admin/cron.log 2>&1' > /etc/cron.d/backup_game_data
 
 # Also run a backup at server shutdown
 cat <<EOF > /etc/systemd/system/backup_game_data_on_shutdown.service
